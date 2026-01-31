521991404be03ce89c30ea63819a098003176663
hjc25 <hjc25@ic.ac.uk>
2026-01-31 16:56:31 +0000

    Fix rally event code

--- logic/events.py
+++ logic/events.py
@@ -4,26 +4,34 @@ from data.normalisedframe import NormalisedFrame
 from logic.perspective import TENNIS_COURT_LENGTH
 
 threshold = 0.1
+NET_POS = TENNIS_COURT_LENGTH / 2
+
 
 class EventInterface(ABC):
     @abstractmethod
-    def test_event(self, frames: FrameStack):
+    def test_event(self, frames: "FrameStack"):
         pass
 
-class RallyEvent:
-    def test_event(self, frames: FrameStack):
-        frames_sided = map(self.isRightOfNet, frames.takeFrames(2))
+
+class RallyEvent(EventInterface):  # Added inheritance
+    def test_event(self, frames: "FrameStack"):
+        frames_sided = [self.isRightOfNet(f) for f in frames.takeFrames(2)]
+
         return any(frames_sided) and not all(frames_sided)
-        # take 2 frames
-        # if the ball has changed side of the net then return true
 
-    def isRightOfNet(self, frame: NormalisedFrame):
+    def isRightOfNet(self, frame: "NormalisedFrame"):
         ball_positions = frame.ball.pos
-        return abs(ball_positions.x - (TENNIS_COURT_LENGTH/2)) < threshold
+        return ball_positions.x > NET_POS
 
-class BounceOrShotEvent:
-    def test_event(self, frames: FrameStack):
-        pass
-        # take the last 0.05 seconds
-        # if the ball has not changed sign but changed direction by more than some tolerance k then we have a bounce
-        # if not we have a shot
+
+class BounceOrShotEvent(EventInterface):  # Added inheritance
+    def test_event(self, frames: "FrameStack"):
+        # Logic: take the last 0.05 seconds
+        # if the ball has not changed sign but changed direction by more than
+        # some tolerance k then we have a bounce; if not we have a shot
+        return False
+
+
+# --- Instances ---
+rally_event = RallyEvent()
+bounce_or_shot_event = BounceOrShotEvent()
