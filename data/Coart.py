db18c4d5ed702e314309010510b8c0bd23d422c3
Lawson Lam <109973600+lawsonlam07@users.noreply.github.com>
2026-01-31 15:01:21 +0000

    Delete data/Coart.py
    typo

--- data/Coart.py
+++
@@ -1,8 +0,0 @@
-from Coord import Coord
-
-class Court:
-  def __init__(self, tl : Coord, tr : Coord, br : Coord, bl : Coord):
-    self.tl = tl
-    self.tr = tr
-    self.br = br
-    self.bl = bl
