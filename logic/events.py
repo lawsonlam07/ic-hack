ca6fb3c284ae66e81792d768697c10e6eca36e67
hjc25 <hjc25@ic.ac.uk>
2026-01-31 17:09:52 +0000

    More work on event detection

--- logic/events.py
+++ logic/events.py
@@ -1,4 +1,6 @@
 from abc import abstractmethod, ABC
+from enum import Enum
+
 from data.framestack import FrameStack
 from data.normalisedframe import NormalisedFrame
 from logic.perspective import TENNIS_COURT_LENGTH
@@ -6,14 +8,29 @@ from logic.perspective import TENNIS_COURT_LENGTH
 threshold = 0.1
 NET_POS = TENNIS_COURT_LENGTH / 2
 
-
 class EventInterface(ABC):
+    @abstractmethod
+    def to_string(self):
+        pass
+
+class EventTesterInterface(ABC):
     @abstractmethod
     def test_event(self, frames: FrameStack):
         pass
 
+class RallyEvent(EventInterface):
+    def to_string(self):
+        return "rally"
 
-class RallyEvent(EventInterface):  # Added inheritance
+class BounceEvent(EventInterface):
+    def to_string(self):
+        return "bounce"
+
+class ShotEvent(EventInterface):
+    def to_string(self):
+        return "shot"
+
+class RallyEventTester(EventTesterInterface):
     def test_event(self, frames: FrameStack):
         frames_sided = [self.isRightOfNet(f) for f in frames.takeFrames(2)]
 
@@ -21,17 +38,50 @@ class RallyEvent(EventInterface):  # Added inheritance
 
     def isRightOfNet(self, frame: NormalisedFrame):
         ball_positions = frame.ball.pos
-        return ball_positions.x > NET_POS
+        if ball_positions.x > NET_POS:
+            return RallyEvent()
+        else:
+            return None
 
 
-class BounceOrShotEvent(EventInterface):  # Added inheritance
+class BounceOrShotEventTester(EventTesterInterface):
+    def __init__(self, _is_internal=False):
+        if not _is_internal:
+            raise PermissionError("Constructor is private. Use BounceOrShotEvent.create()")
+
+    @classmethod
+    def create(cls):
+        return cls(_is_internal=True)
+
     def test_event(self, frames: FrameStack):
-        # Logic: take the last 0.05 seconds
-        # if the ball has not changed sign but changed direction by more than
-        # some tolerance k then we have a bounce; if not we have a shot
-        return False
+        # Take the last 3 frames to get two segments of movement
+        recent = frames.takeFrames(3)
+        if len(recent) < 3:
+            return None
+
+        # Calculate horizontal velocity (relative to net)
+        # v1: Velocity before the middle frame
+        v1_x = recent[1].ball.pos.x - recent[0].ball.pos.x
+        # v2: Velocity after the middle frame
+        v2_x = recent[2].ball.pos.x - recent[1].ball.pos.x
+
+        # Logic: If the sign of velocity changed, the ball reversed direction (A Shot)
+        # If the sign is the same, but there was a 'spike' in data, it's a Bounce
+        changed_sign = (v1_x > 0) != (v2_x > 0)
+
+        if changed_sign:
+            return ShotEvent()  # Ball reversed direction relative to net
+
+        # If direction didn't change, we check for a 'k' tolerance
+        # (e.g., a sudden drop in speed) to identify a bounce
+        speed_ratio = abs(v2_x) / abs(v1_x) if abs(v1_x) > 0 else 1
+
+        if speed_ratio < 0.8:  # Threshold 'k' for energy loss on bounce
+            return BounceEvent()
+
+        return None
 
 
-# --- Instances ---
-rally_event = RallyEvent()
-bounce_or_shot_event = BounceOrShotEvent()
+class EventTesters(Enum, EventTesterInterface):
+    RALLY = RallyEventTester()
+    BOUNCE_OR_SHOT = BounceOrShotEventTester()
