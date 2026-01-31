92b696337d76e1109dcdec9e5d86b2368f8b9b02
td725 <tom.davies25@imperial.ac.uk>
2026-01-31 16:17:58 +0000

    added methods to FrameStack: push, dequeue, takeSeconds, takeFrames

--- data/FrameStack.py
+++ data/FrameStack.py
@@ -1,6 +1,21 @@
 from .Frame import Frame
 
 class FrameStack:
-  def __init__(self):
+  def __init__(self, fps : int):
     self.elements = []
     self.topPointer = -1
+    self.fps = fps
+
+  def push(self, frame : Frame):
+    self.elements.append(frame)
+
+  def dequeue(self):
+    return self.elements.pop(0)
+  
+  def takeSeconds(self, seconds: int):
+    #Returns the most recent frames covering the last `seconds`.
+    num_frames = seconds * self.fps
+    return self.elements[-num_frames:]
+
+  def takeFrames(self, noFrames : int):
+    return self.elements[-noFrames:]
