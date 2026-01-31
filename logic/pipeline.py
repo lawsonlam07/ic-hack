6452a99df88c2de9853afa47049d1df7648ee200
hjc25 <hjc25@ic.ac.uk>
2026-01-31 18:12:36 +0000

    Pipeline framwork

---
+++ logic/pipeline.py
@@ -0,0 +1,12 @@
+def process_frames():
+    while True:
+        pass
+        # Receive next frame
+        # Normalise
+        # Push onto frame stack
+        # Iterate through event testers and pass frame stack
+        # Pass first event for which the test passes to an event frame
+        # Push to the event ordering
+
+    # Merge consecutive events
+    # Pass
